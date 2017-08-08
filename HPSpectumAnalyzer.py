56f469e06b78e399055f148800c2954be781bde9
Thomas Rometsch <thomas@Yavin.local>
2017-08-08 18:09:45 +0200

    add an abstract representation of a HP8591E spectrum analyzer

---
+++ HPSpectumAnalyzer.py
@@ -0,0 +1,25 @@
+#-----------------------------------------------------------
+#	
+#	Wrapper class to provide an abstract representation
+#	of an HP8591E Spectrum Analyzer
+#
+#	Author	:	Thomas Rometsch
+#	Date	:	Aug. 8, 2017
+#	
+#-----------------------------------------------------------
+import gpib
+
+class SpectrumAnalyzer:
+	# Name and address of the gpib instrument as set in /etc/gpib.conf
+	gpib_lib_name = "HP82357B";
+	gpib_address = 18;
+
+	def __init__(self):
+		# Try to find the gpib device.
+		self.dev = gpib.find(self.gpib_lib_name);
+		# Reset dev
+		self.reset();
+
+	def reset(self):
+		# Reset device with "IP"
+		gpib.write(self.dev, "IP");
